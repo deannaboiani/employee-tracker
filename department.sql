e0c52181ebbb1357f535bc1412fef5633ae51817
DeAnna Boiani <deannaboiani@gmail.com>
2021-10-22 10:52:24 -0700

    when user is prompted and chooses view all depts, a table is given with that data

---
+++ department.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (id, name)
+VALUES ("1", "sales"),
+("2","engineering"),
+("3", "finance"),
+("4", "legal");
--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-const inquirer = require('inquirer');
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+const PORT = 3000;
+const inquirer = require("inquirer");
+// const department = require('./lib/department');
+// const role = require('./lib/role');
+// const employee = require('./lib/employee');
+const cTable = require("console.table");
+const mysql = require("mysql2");
+
+// create connection to database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "team_db",
+});
+
+// prompts user with options to choose
+function start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Choose an option:",
+        name: "options",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      if (data.choice === "View all departments"){
+
+      }
+      // viewDept();
+      db.query("SELECT * FROM department",(err,data)=>{
+        if(err){
+          throw err
+        } else {
+          console.log(data);
+          db.end();
+        }
+      })
+    });
+}
+
+// function viewDept() {
+//   if (options == "View all departments") {
+//     connection.query(
+//       "SELECT * FROM `department`",
+//       function (err, results, fields) {
+//         console.log(results);
+//         console.log(fields);
+//       }
+//     );
+//   }
+// }
+start();
+
+// module.exports = index;
