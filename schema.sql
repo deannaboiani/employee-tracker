376c9c4bf40dedfbd91d582b083885a4020386d3
DeAnna Boiani <deannaboiani@gmail.com>
2021-10-21 19:21:39 -0700

    tables built, inquirer installed

---
+++ schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS team_db;
+CREATE DATABASE team_db;
+
+USE team_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
