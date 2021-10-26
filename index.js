58aae9e32d99e7174e59219a96277a0c7840ec73
DeAnna Boiani <deannaboiani@gmail.com>
2021-10-25 21:22:25 -0700

    all prompts are responding with correct information and updates

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
 const inquirer = require("inquirer");
-// const role = require('./lib/role');
-// const employee = require('./lib/employee');
 const cTable = require("console.table");
 const mysql = require("mysql2");
-const Department = require("./lib/department");
-const deptArray = [];
 
 
 // create connection to database
@@ -32,7 +28,8 @@ function start() {
           "Add a department",
           "Add a role",
           "Add an employee",
-          "Update an employee role"
+          "Update an employee role",
+          "Exit"
         ]
       }
     ])
@@ -57,6 +54,19 @@ function start() {
       } else if (data.choice === "Add a role") {
 
         addRoles();
+
+      } else if (data.choice === "Add an employee") {
+
+        addEmployee();
+
+      } else if (data.choice === "Update an employee role") {
+        
+        updateRole();
+     
+      } else if (data.choice === "Exit") {
+
+        db.end();
+        console.log("Goodbye")
 
       }
     })
@@ -103,6 +113,7 @@ function viewEmployees() {
   })
 }
 
+// adds department to table based on user input of name
 function addDepartment() {
   inquirer.prompt([
     {
@@ -117,8 +128,7 @@ function addDepartment() {
         throw err
         console.log(err)
       } else {
-        console.log('done');
-        // console.table(Department);
+        console.log('Department has been added');
         start();
       };
     })
@@ -126,6 +136,7 @@ function addDepartment() {
 
 }
 
+// adds a role to table based on user input of title, salary, and department id
 function addRoles() {
   inquirer.prompt([
     {
@@ -150,7 +161,7 @@ function addRoles() {
         throw err
         console.log(err)
       } else {
-        console.log('done');
+        console.log('Role has been added');
         start();
       };
     })
@@ -158,5 +169,70 @@ function addRoles() {
 
 }
 
+// adds employee to table based on name, role id and manager id
+function addEmployee() {
+  inquirer.prompt([
+    {
+      name: "first_name",
+      message: "What is first name of the employee you want to add?",
+      type: "input"
+    },
+    {
+      name: "last_name",
+      message: "What is last name of the employee you want to add?",
+      type: "input"
+    },
+    {
+      name: "role_id",
+      message: "What role id does this employee belong to?",
+      type: "number"
+    },
+    {
+      name: "manager_id",
+      message: "What is this employee's manager id number?",
+      type: "number"
+    }
+  ]).then((ans) => {
+    const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);`;
+    db.query(query, [ans.first_name, ans.last_name, ans.role_id, ans.manager_id], (err, data) => {
+      if (err) {
+        throw err
+        console.log(err)
+      } else {
+        console.log('Employee has been added');
+        start();
+      };
+    })
+  })
+
+}
+
+function updateRole() {
+  inquirer.prompt([
+    {
+      name: "idStart",
+      message: "What is the id of the employee you would like to update?",
+      type: "number"
+    },
+    {
+      name: "roleIdNew",
+      message: "What is the new role id of the employee?",
+      type: "number"
+    }
+
+  ]).then((ans) => {
+    const query = `UPDATE employee SET role_id=? WHERE id=?;`;
+    db.query(query, [ans.roleIdNew, ans.idStart], (err, data) => {
+      if (err) {
+        throw err;
+        console.log(err);
+      } else {
+        console.log("Employee role updated")
+        start();
+      }
+    })
+
+  })
+}
 
 start();
